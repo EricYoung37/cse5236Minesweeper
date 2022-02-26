e174179693402a0d5b1039e4006f9219ee4ca31b
Eric <young.3156@buckeyemail.osu.edu>
2022-02-25 21:46:39 -0500

    Added fragment for leaderboard.

---
+++ app/src/main/java/com/cse5236/minesweeper/LeaderBoard.java
@@ -0,0 +1,53 @@
+package com.cse5236.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class LeaderBoard extends AppCompatActivity {
+
+    Button leaderScoreBtn, leaderSpeedBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leader_board);
+
+        leaderScoreBtn = findViewById(R.id.leader_score);
+        leaderSpeedBtn = findViewById(R.id.leader_speed);
+
+        leaderScoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                replaceFragment(new LeaderScore());
+
+            }
+        });
+
+        leaderSpeedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                replaceFragment(new LeaderSpeed());
+
+            }
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment newFrag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.leader_board_frag, newFrag);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/cse5236/minesweeper/LeaderScore.java
@@ -0,0 +1,22 @@
+package com.cse5236.minesweeper;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeaderScore extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_leader_score, container, false);
+        return view;
+    }
+}
