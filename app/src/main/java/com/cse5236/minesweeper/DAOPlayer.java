46a8c068d31b46ad29285def40f3b7e0dfda0183
Eric <young.3156@buckeyemail.osu.edu>
2022-03-30 14:40:09 -0400

    Modified DAOPlayer so that now the creation and the update of a leaderboard record happens in the database table that corresponds to difficulty.

--- app/src/main/java/com/cse5236/minesweeper/DAOPlayer.java
+++ app/src/main/java/com/cse5236/minesweeper/DAOPlayer.java
@@ -1,5 +1,7 @@
 package com.cse5236.minesweeper;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,8 +19,11 @@ public class DAOPlayer {
 
     public DAOPlayer()
     {
+        Difficulty difficulty = Difficulty.getInstance();
+        String path = LeaderBoard.class.getSimpleName()+"/"+difficulty.getDifficulty();
+
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        tableRef = db.getReference(Player.class.getSimpleName());
+        tableRef = db.getReference(path);
     }
 
     /* Create or Update a record depending on the existence of the player name */
@@ -46,6 +51,29 @@ public class DAOPlayer {
 
             }
         });
+
+//        /* orderByChild("name") only means you get a copy of data ordered by name but not ordering the original data */
+//        tableRef.orderByChild("name").equalTo(p.getName()).addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                if (snapshot.exists()) {
+//                    /* If name exists, update */
+//                    for (DataSnapshot child : snapshot.getChildren()) {
+//                        String key = child.getKey();
+//                        tableRef.child(key).setValue(p);
+//                    }
+//                } else {
+//                    /* If name does not exist, create a new node */
+//                    tableRef.push().setValue(p);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
     }
 
     public void delete(Player p) {
